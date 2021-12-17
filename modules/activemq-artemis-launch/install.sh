3b74dd551d2c3aa674023b93f9f502a8efcfc9b2
Domenico Francesco Bruscino <brusdev@apache.org>
2021-12-17 15:58:34 +0100

    Refactor activemq.artemis.launch module

--- modules/activemq-artemis-launch/install.sh
+++ modules/activemq-artemis-launch/install.sh
@@ -4,24 +4,24 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 SOURCES_DIR="/tmp/artifacts"
-
-# Add OpenShift PING implementation
-VERSION="1.2.6.Final"
-
 DEST=$AMQ_HOME
 
 mkdir -p ${DEST}
 mkdir -p ${DEST}/conf/
 
-cp -p ${SOURCES_DIR}/openshift-ping-common-$VERSION.jar \
-  ${SOURCES_DIR}/openshift-ping-dns-$VERSION.jar \
-  ${SOURCES_DIR}/netty-tcnative-2.0.42.Final-linux-x86_64-fedora.jar \
+cp -p ${SOURCES_DIR}/openshift-ping-common*.jar \
+  ${SOURCES_DIR}/openshift-ping-dns*.jar \
+  ${SOURCES_DIR}/netty-tcnative*.jar \
   ${DEST}/lib
 
 cp -p $ADDED_DIR/jgroups-ping.xml \
   ${DEST}/conf/
 
-cp $ADDED_DIR/launch.sh ${ADDED_DIR}/readinessProbe.sh ${ADDED_DIR}/drain.sh $AMQ_HOME/bin
+cp $ADDED_DIR/launch.sh \
+  ${ADDED_DIR}/readinessProbe.sh \
+  ${ADDED_DIR}/drain.sh \
+  $AMQ_HOME/bin
+
 chmod 0755 $AMQ_HOME/bin/launch.sh
 chmod 0755 $AMQ_HOME/bin/readinessProbe.sh
 chmod 0755 $AMQ_HOME/bin/drain.sh

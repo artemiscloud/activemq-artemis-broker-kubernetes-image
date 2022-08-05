8c4089130f978592e53f7d67120a048982018420
Domenico Francesco Bruscino <brusdev@apache.org>
2022-08-05 20:27:39 +0200

    [#41] Enable ForwardedRequestCustomizer jetty customizer

--- modules/activemq-artemis-launch/added/launch.sh
+++ modules/activemq-artemis-launch/added/launch.sh
@@ -680,6 +680,8 @@ function configure() {
       injectMetricsPlugin ${instanceDir}
     fi
 
+    sed -i 's/<web/<web customizer="org.eclipse.jetty.server.ForwardedRequestCustomizer"/' ${instanceDir}/etc/bootstrap.xml
+
     $AMQ_HOME/bin/configure_s2i_files.sh ${instanceDir}
     $AMQ_HOME/bin/configure_custom_config.sh ${instanceDir}
 
